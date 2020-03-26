4dbf19ea193c320ef7f3906b561b1f4b653925e7
trinahaque <trina_haque28@outlook.com>
2020-03-25 19:24:09 -0700

    WordBlast hashmap

--- SecondAssignment/WordBlast.c
+++ SecondAssignment/WordBlast.c
@@ -1,8 +1,103 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+pthread_mutex_t lock;
+
+// typedef struct node{
+//     char * key;
+//     int val;
+//     struct node *next;
+// } *node;
+
+struct node{
+    char * key;
+    int val;
+    struct node *next;
+};
+
+struct table{
+    int size;
+    struct node **list;
+};
+
+struct table *createTable(int size){
+    struct table *t = (struct table*)malloc(sizeof(struct table));
+    t->size = size;
+    t->list = (struct node**)malloc(sizeof(struct node*)*size);
+    int i;
+    for(i=0;i<size;i++)
+        t->list[i] = NULL;
+    return t;
+}
+
+
+int hashCode(struct table *t, char *key) {
+    unsigned long int hash_value = 0;
+    unsigned int i;
+    unsigned int key_len = strlen(key);
+
+    // do several rounds of multiplication
+    for (i = 0; i < key_len; ++i) {
+        hash_value = hash_value * 37 + key[i];
+    }
+
+    // make sure value is 0 <= value < TABLE_SIZE
+    hash_value = hash_value % t->size;
+
+    return hash_value;
+}
+void insert(struct table *t, char * key, int val){
+    int pos = hashCode(t,key);
+    struct node *list = t->list[pos];
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    struct node *temp = list;
+    while(temp){
+        if(temp->key==key){
+            temp->val = val;
+            return;
+        }
+        temp = temp->next;
+    }
+    newNode->key = key;
+    newNode->val = val;
+    newNode->next = list;
+    t->list[pos] = newNode;
+}
+
+int lookup(struct table *t, char * key){
+    int pos = hashCode(t,key);
+    struct node *list = t->list[pos];
+    struct node *temp = list;
+    while(temp){
+        if(temp->key==key){
+            return temp->val;
+        }
+        temp = temp->next;
+    }
+    return -1;
+}
+// node lookup(struct table *t, char * key){
+//     int pos = hashCode(t,key);
+//     struct node *list = t->list[pos];
+//     struct node *temp = list;
+//     while(temp){
+//         if(temp->key==key){
+//             return temp;
+//         }
+//         temp = temp->next;
+//     }
+//     return NULL;
+// }
 
 
 // needs struct anytime passing in multiple data type
@@ -10,6 +105,7 @@ typedef struct fileInfo {
     char * filename;
     long start;
     long size;
+    struct table *t;
 } fileInfo, * fileInfo_p;
 
 
@@ -25,8 +121,12 @@ void * countWords(void * arg){
     fileInfo_p fileInfo = (fileInfo_p) arg;
     char * stringToken;
     char * saveptr;
+    int totalWords;
+    struct table *t = fileInfo->t;
 
-    wordArray = malloc(fileInfo->size+4);
+    totalWords = 0;
+
+    wordArray = malloc(fileInfo->size + 1);
     // open the file
     file = open(fileInfo->filename, O_RDONLY);
     lseek(file, fileInfo->start, SEEK_SET);
@@ -38,25 +138,39 @@ void * countWords(void * arg){
     // null terminator
     wordArray[fileInfo->size] = 0;
 
-    // segmentation error when I used **saveptr
-
-   
-    
-
  
     stringToken = strtok_r(wordArray, delimeter, &saveptr);
-    // segmentation error when I try to print stringToken
-    printf("%s",stringToken);
+
+    // struct table *t = createTable(5);
     
-    // do{
-    //     if (strlen(stringToken) > 5){
-    //         printf("word is: %s\n", stringToken);
-    //     }
-    //     stringToken = strtok_r(NULL, delimeter, &saveptr);
-    // }while(stringToken);
-    
+
+    do{
+        if (strlen(stringToken) > 5){
+            totalWords += 1;
+            int tmp = lookup(t, stringToken);
+            // node tmp = lookup(t, stringToken);
+            if (tmp == -1){
+                pthread_mutex_lock(&lock);
+                insert(t, stringToken, 1);
+                pthread_mutex_unlock(&lock);
+            }
+            else{
+                pthread_mutex_lock(&lock);
+                // insert(t, stringToken, tmp->val + 1);
+                insert(t, stringToken, tmp + 1);
+                // tmp->val += 1;
+                pthread_mutex_unlock(&lock);
+            }
+            printf("word is: %s\n", stringToken);
+        }
+        stringToken = strtok_r(NULL, delimeter, &saveptr);
+    }while(stringToken);
+
+    // printf("total word is %d\n", totalWords);
     
     // free is clearing up the memory
+    // free(hash_table);
+
     free(wordArray);
     close(file);
     return NULL;
@@ -74,6 +188,13 @@ int main(int argc, char** argv){
     char * fileName;
     pthread_t * threadArray;
     fileInfo * fileArray;
+    struct table *t = createTable(1000);
+    // init_hash_table();
+
+    if (pthread_mutex_init(&lock, NULL) != 0){
+        printf("\n mutex init failed\n");
+        return 1;
+    }
 
     if (argc < 3){
         printf("Needs two arguments: filename and threadcount!\n");
@@ -95,7 +216,7 @@ int main(int argc, char** argv){
     lseek(file, 0, SEEK_END);
     fileSize = lseek(file, 0, SEEK_CUR);
     lseek(file, 0, SEEK_SET);
-    printf("%d: size\n", fileSize);
+    printf("%ld: size\n", fileSize);
     close(file);
 
     struct timespec startTime;
@@ -104,7 +225,7 @@ int main(int argc, char** argv){
     // allocate thread array
     // malloc returns a void pointer
     // void pointer can point to any particular type of data type we need
-    fileArray = malloc (sizeof(fileInfo));
+    fileArray = malloc (sizeof(fileInfo) * numberOfThreads);
     // creates an array of p_thread id
     threadArray = malloc (sizeof(pthread_t) * numberOfThreads);
 
@@ -113,10 +234,12 @@ int main(int argc, char** argv){
         // and pass that info instead of changing the info of the same pointer
         fileArray[i].start = (fileSize / numberOfThreads) * i;
         if (i == numberOfThreads - 1){
+            fileArray[i].t = t;
             fileArray[i].filename = fileName;
             fileArray[i].size = fileSize - fileArray[i].start;
         }
         else {
+            fileArray[i].t = t;
             fileArray[i].filename = fileName;
             fileArray[i].size = (fileSize / numberOfThreads);
         }
@@ -130,12 +253,22 @@ int main(int argc, char** argv){
         pthread_create(&(threadArray[i]), NULL, countWords, &(fileArray[i]));
     }
 
+
+
     // pthread_join makes the function to wait until the thread is done
     for(int i = 0; i < numberOfThreads; i++){
         pthread_join(threadArray[i], NULL);
     }
-
-
+    // printf("hashtable size %d", t->size);
+    // for (int i = 0; i < t-> size; i++){
+    //     struct node *list = t->list[i]; // Array of node list
+    //     struct node *temp = list; // list gives a single node 
+    //     while(temp){
+    //         printf("%s ",temp->key);
+    //         printf("%d\n",temp->val);
+    //         temp = temp->next;
+    //     }
+    // }
 
     clock_gettime(CLOCK_REALTIME, &endTime);
     time_t sec = endTime.tv_sec - startTime.tv_sec;
@@ -146,8 +279,9 @@ int main(int argc, char** argv){
     n_sec = n_sec + 1000000000L;
     }
     printf("Total Time was %ld.%09ld seconds\n", sec, n_sec);
+    pthread_mutex_destroy(&lock);
+    free(t);
     
-
     return 0;
 }
   
